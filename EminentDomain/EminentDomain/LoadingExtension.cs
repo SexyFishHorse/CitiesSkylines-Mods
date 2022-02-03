f0383fb0c467d8b4e064083632111aceedaca494
Asbjørn Hansen <ash@moviestarplanet.com>
2022-02-03 21:30:52 +0100

    add new mod "EminentDomain" for unlocking all 25 areas

---
+++ EminentDomain/EminentDomain/LoadingExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace EminentDomain
+{
+    using ICities;
+    using SexyFishHorse.CitiesSkylines.Infrastructure.Extensions;
+
+    public class LoadingExtension : LoadingExtensionBase
+    {
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            if (mode.IsGameOrScenario())
+            {
+                GameAreaManager.instance.m_maxAreaCount = 25;
+            }
+        }
+    }
+}
---
+++ EminentDomain/EminentDomain/UserMod.cs
@@ -0,0 +1,19 @@
+﻿namespace EminentDomain
+{
+    using SexyFishHorse.CitiesSkylines.Infrastructure;
+
+    public class UserMod : UserModBase
+    {
+        public const string ModName = "Eminent Domain";
+
+        public override string Description
+        {
+            get { return "Unlock all 25 tiles on the last milestone"; }
+        }
+
+        public override string Name
+        {
+            get { return ModName; }
+        }
+    }
+}
